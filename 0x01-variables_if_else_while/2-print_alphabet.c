3915cef39f15fa52973a71fb55f8d6fe33a77776
Debbiieee <AWWNWC@pte.hu>
2022-07-01 16:20:15 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,15 +7,13 @@
 */
 int main(void)
 {
-char c = 'a';
-			
+char c = 'a';		
 while (c <= 'z')
 {
 putchar(c);
 										c++;
 											}
 
-putchar('\n');
-					
+putchar('\n');					
 return (0);
 }
