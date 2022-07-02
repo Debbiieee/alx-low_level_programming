9fe8f75d298fafe816bfb9a55df899d345c43ff2
Debbiieee <AWWNWC@pte.hu>
2022-07-02 14:42:23 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,47 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
-* main - main block
-* Description: Write a program that prints all possible
-* different combinations of two digits
-* Numbers must be separated by ,, followed by a space
-* The two digits must be different
-* 01 and 10 are considered the same combination of the two digits 0 and 1
-* Print only the smallest combination of two digits
-* Numbers should be printed in ascending order, with two digits
-* You can only use the putchar function
-* (every other function (printf, puts, etc…) is forbidden)
-* You can only use putchar five times maximum in your code
-* You are not allowed to use any variable of type char
-* All your code should be in the main function
-* Return: 0
-*/
+ * main - print double digit combos
+ *
+ * Description: print double digit combos
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-		int c;
-			int d = 0;
+	int i, j;
 
-				while (d < 10)
-						{
-									c = 0;
-											while (c < 10)
-														{
-																		if (d != c && d < c)
-																						{
-																											putchar('0' + d);
-																															putchar('0' + c);
+	i = 48;
+	j = 48;
 
-																																			if (c + d != 17)
-																																								{
-																																														putchar(',');
-																																																			putchar(' ');
-																																																							}
-																																						}
+	while (i < 58)
+	{
 
-																					c++;
-																							}
-													d++;
-														}
-					putchar('\n');
-						return (0);
+		j = i + 1;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i < 56 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
 }
