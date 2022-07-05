78f9f7d7579674982621a78a14a2421102864bfe
Debbiieee <AWWNWC@pte.hu>
2022-07-05 07:51:14 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,7 @@
 #include "main.h"
-
 /**
  * main -  prints Putchar
- *
  * Description: prints _putchar
- *
  * Return: 0 (Success)
  */
 int main(void)
@@ -18,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
