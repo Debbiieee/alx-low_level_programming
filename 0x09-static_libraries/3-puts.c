b391d04a74926d38a56beeecd4e08148c2b989f8
Debbiieee <AWWNWC@pte.hu>
2022-07-22 16:06:44 -0700

    my commit

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,5 @@
 #include "main.h"
-/**
- * _puts - prints a string
- * @str: string to print
- *
- * Description: prints a string
- * On success: return the number of characters printed
- */
-
-void _puts(char *str)
+void _puts(char *s)
 {
-	while (*str)
-		_putchar(*str++);
-
-	_putchar('\n');
+	_putchar('0');
 }
