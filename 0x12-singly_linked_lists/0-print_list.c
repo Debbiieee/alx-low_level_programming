33a99e0672834e6e8d3124b6977bdd87a3e2eef8
Debbiieee <AWWNWC@pte.hu>
2022-08-11 03:31:13 -0700

    my commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,9 +4,11 @@
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t nelem;
+
 	nelem = 0;
 	while (h != NULL)
 	{
@@ -19,3 +21,4 @@ size_t print_list(const list_t *h)
 	}
 	return (nelem);
 }
+ 
